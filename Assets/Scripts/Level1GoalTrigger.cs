b70436e9a2e5c0433e5e0a6bb6bd97074ba69cc4
Julian-Belza <145355671+Julian-Belza@users.noreply.github.com>
2023-11-29 10:04:50 -0500

    Level 1 Design wise is done yall git :weary:

--- Assets/Scripts/Level1GoalTrigger.cs
+++ Assets/Scripts/Level1GoalTrigger.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainTrigger : MonoBehaviour
+public class Level1GoalTrigger : MonoBehaviour
 {
-    [SerializeField] private Animator Level1WallInAnim = null;
-    [SerializeField] private bool wallEneter = false;
-
+    [SerializeField] public Animator Level1GoalTriggerAnim = null;
+    [SerializeField] private bool goalEnter = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +17,11 @@ public class TerrainTrigger : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter(Collider other)
+     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-            Level1WallInAnim.Play("Level1WallInAnim",0,0.0f);
+            Level1GoalTriggerAnim.Play("Level1GoalTriggerAnim",0,0.0f);
             gameObject.SetActive(false);
         }
     }
